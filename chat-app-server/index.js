866f276e5a0431a96b2a45493a02c91b759a60df
david-rai <davidkhaling123@gmail.com>
2025-11-02 13:51:26 +0545

    Chore:Single file upload with multer moore optimized

--- chat-app-server/index.js
+++ chat-app-server/index.js
@@ -1,23 +1,24 @@
-const express=require('express')
-const http=require("http")
-const path=require('path')
-const fs=require('fs')
-const multer=require("multer")
-const {Server}=require('socket.io')
-const cors=require("cors")
-require("dotenv").config()
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+const multer = require("multer");
+const { Server } = require("socket.io");
+const cors = require("cors");
+require("dotenv").config();
 
-const app=express()
-const server=http.createServer(app)
-const io=new Server(server,{
-    cors:{
- origin: '*'
-    }
-})
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
 //Middlewares
-app.use(cors())
+app.use(cors());
 
+//Storage for the multer
 const storage = multer.diskStorage({
   destination: "uploads/",
   filename: (req, file, cb) => {
@@ -25,7 +26,6 @@ const storage = multer.diskStorage({
     cb(null, `${Date.now()}${ext}`);
   },
 });
-
 
 const upload = multer({ storage });
 
@@ -44,40 +44,39 @@ app.post("/upload", upload.single("file"), (req, res) => {
   });
 });
 
-
 //web sockets
-const roomName="chat-room"
-io.on("connection",client=>{
-    console.log('new client',client.id)
+const roomName = "chat-room";
+io.on("connection", (client) => {
+  console.log("new client", client.id);
 
-    //Getting the name and joining the room
-    client.on("name",async (name)=>{
-        client.username=name
-        client.join(roomName)
-       client.to(roomName).emit('join-message',name)
-    })
+  //Getting the name and joining the room
+  client.on("name", async (name) => {
+    client.username = name;
+    client.join(roomName);
+    client.to(roomName).emit("join-message", name);
+  });
 
-    //Getting the message form client along with the username
-    client.on("send-message",async ({message,name})=>{
-        console.log(`New messagge from ${name} that is ${message}`)
-        io.to(roomName).emit("message",{message:message,sender_id:client.id,sender_name:name})
-    })
+  //Getting the message form client along with the username
+  client.on("send-message", async ({ message, name }) => {
+    console.log(`New messagge from ${name} that is ${message}`);
+    io.to(roomName).emit("message", {
+      message: message,
+      sender_id: client.id,
+      sender_name: name,
+    });
+  });
 
-    //sending the message on disconnect
-    client.on('disconnect',async ()=>{
-        // const deletedUser=await userModel.findOneAndDelete({user_id:client.id})
-        io.to(roomName).emit("leave-message",client.username)
-    })
-    
-})
+  //sending the message on disconnect
+  client.on("disconnect", async () => {
+    // const deletedUser=await userModel.findOneAndDelete({user_id:client.id})
+    io.to(roomName).emit("leave-message", client.username);
+  });
+});
 
-      //Routes for routing
-app.get('/',(req,res)=>{
-    res.send("home page ho hai")
-})
+//Routes for routing
+app.get("/", (req, res) => {
+  res.send("home page ho hai");
+});
 
-const port=1111
-server.listen(port,()=> console.log("server is live"))
-
-
-
+const port = 1111;
+server.listen(port, () => console.log("server is live"));
